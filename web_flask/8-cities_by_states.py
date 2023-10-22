d0a2fb1279f119c253e89d3f1e357a7d9ec63003
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-22 15:21:54 +0100

    Added sort to data in 7-states_list.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,11 +11,11 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def list_states():
-    """list of all State objects present in DBStorage"""
-    states = sorted(storage.all("State"), key=lambda state: state.name)
-    return render_template("7-states_list.html", states=states)
+@app.route("/cities_by_states", strict_slashes=False)
+def list_cities_by_states():
+    """list of all Cities by States as present in DBStorage"""
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
