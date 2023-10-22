918cd8efd3bacf692589de2fa03282dc25ee80e1
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-22 09:09:37 +0100

    Updated: 0-hello_route.py

--- 0-hello_route.py
+++ 0-hello_route.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
-"""Mdule doc"""
-if '__name__' == '__main__':
-    print
+"""Initiates a Flask web app
+   Listens to 0.0.0.0 on 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host=0.0.0.0)
