a179e9978a42f14b3cb99ad67ad5b88eb4a13c76
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-22 10:56:07 +0100

    fixed PEP8 issue in 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """Initiates a Flask web app
    Listens to 0.0.0.0 on port 5000
 """
@@ -7,16 +8,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_root():
     """Displays Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """Displays  HBNB"""
     return 'HBNB'
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
