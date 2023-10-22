79955cd77b0433bb4cdb61b7cb53fa94effca578
Abubakar Adam Sani <Athernee@gmail.com>
2023-10-22 17:05:24 +0100

    modifed: 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route("/cities_by_states", strict_slashes=False)
 def list_cities_by_states():
     """list of all Cities by States as present in DBStorage"""
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
